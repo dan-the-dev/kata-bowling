5e4c14140a459c38864df02d424f225be4b5b9c5
danielescillia <daniele.scillia@gmail.com>
2021-09-28 17:09:03 +0200

    feat: implemented roll method

--- src/Kata/Bowling.php
+++ src/Kata/Bowling.php
@@ -5,4 +5,18 @@ namespace Kata;
 class Bowling
 {
 
+    /**
+     * @var int
+     */
+    private $score;
+
+    public function score()
+    {
+        return $this->score;
+    }
+
+    public function pin(int $int)
+    {
+        $this->score = $int;
+    }
 }
--- tests/Kata/BowlingTest.php
+++ tests/Kata/BowlingTest.php
@@ -14,4 +14,18 @@ class BowlingTest extends TestCase
         $this->bowling = new Bowling();
     }
 
+    public function testRollWith1PinsMeans1Point(): void
+    {
+        $this->bowling->pin(1);
+
+        $this->assertEquals(1, $this->bowling->score());
+    }
+
+    public function testRollWith2PinsMeans2Point(): void
+    {
+        $this->bowling->pin(2);
+
+        $this->assertEquals(2, $this->bowling->score());
+    }
+
 }
